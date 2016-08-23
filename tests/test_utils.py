5eaa71504c83e5bee287f302c6bf187b5c32f75f
Jaroslav Henner <jhenner@redhat.com>
2016-08-23 16:26:00 +0200

    Unicode tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from unittest import TestCase
+
 from miqclient.utils import escape_filter
 
-def test_escape_filter():
-    assert '"ěščřžýáíé"' == escape_filter(b"ěščřžýáíé")
-    assert '"\'"' == escape_filter("'")
-    assert '"\'\'"' == escape_filter("''")
-    assert '"\'\'\'"' == escape_filter("'''")
-    assert "'\"'" == escape_filter('"')
-    assert "'\"\"'" == escape_filter('""')
-    assert "'\"\"\"'" == escape_filter('"""')
-    assert '1' == escape_filter(1)
-    assert 'NULL' == escape_filter(None)
-    assert "''" == escape_filter('')
+class TestUtils(TestCase):
+    def test_escape_filter(self):
+        self.assertEqual('"\'"', escape_filter("'"))
+        self.assertEqual('"\'\'"', escape_filter("''"))
+        self.assertEqual('"\'\'\'"', escape_filter("'''"))
+        self.assertEqual("'\"'", escape_filter('"'))
+        self.assertEqual("'\"\"'", escape_filter('""'))
+        self.assertEqual("'\"\"\"'", escape_filter('"""'))
+        self.assertEqual('1', escape_filter(1))
+        self.assertEqual('NULL', escape_filter(None))
+        self.assertEqual("''", escape_filter(''))
+        self.assertEqual(u'"ěščřžýáíé"', escape_filter('ěščřžýáíé'))
+
