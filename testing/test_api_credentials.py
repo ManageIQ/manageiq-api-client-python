80f06dcba4adb378321518d3b97e941d36022d3d
mkanoor <mkanoor@redhat.com>
2017-03-15 18:21:57 -0400

    Fixed code style issues

--- testing/test_api_credentials.py
+++ testing/test_api_credentials.py
@@ -3,28 +3,32 @@ import mock
 
 from manageiq_client.api import ManageIQClient as MiqApi
 
+
 def test_no_user_password_auth_dict():
     with pytest.raises(KeyError):
         MiqApi('http://www.example.com', {})
+
 
 def test_no_user_password_auth_tuple():
     with pytest.raises(ValueError):
         MiqApi('http://www.example.com', "")
 
+
 def test_no_password_auth():
     with pytest.raises(KeyError):
         MiqApi('http://www.example.com', {'user': 'Fred'})
+
 
 def test_no_user_auth():
     with pytest.raises(KeyError):
         MiqApi('http://www.example.com', {'password': 'secret'})
 
+
 @mock.patch('manageiq_client.api.ManageIQClient._load_data')
 def test_user_password(self):
     MiqApi('http://www.example.com', {'user': 'Fred', 'password': 'secret'})
-    
+
+
 @mock.patch('manageiq_client.api.ManageIQClient._load_data')
 def test_token(self):
     MiqApi('http://www.example.com', {'x-auth-token': 'abcdef12345'})
-    
-     
