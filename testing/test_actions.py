207830256a40615b10de8c9a900510c8f2c0a14f
Martin Kourim <mkourim@redhat.com>
2017-09-19 00:13:32 +0200

    new tests and tests edits

--- testing/test_actions.py
+++ testing/test_actions.py
@@ -2,7 +2,7 @@
 import pytest
 
 from httmock import all_requests, HTTMock
-from manageiq_client.api import ManageIQClient
+from manageiq_client.api import APIException, ManageIQClient
 
 
 API_CONTENT = {
@@ -76,10 +76,24 @@ SERVICES_CONTENT = {
 }
 
 
+NOTFOUND_RESPONSE = {
+    "error": {
+        "kind": "not_found",
+        "message": "Couldn't find Service with 'id'=21",
+        "klass": "ActiveRecord::RecordNotFound"
+    }
+}
+
+
 @pytest.fixture(scope='module')
-def service():
+def api():
     with HTTMock(api_mock):
         api = ManageIQClient('https://example/api', ('admin', 'admin'), verify_ssl=False)
+    return api
+
+
+@pytest.fixture(scope='module')
+def service(api):
     with HTTMock(services_mock):
         services = api.collections.services
         services.reload()
@@ -106,55 +120,101 @@ def api_mock(url, request):
             'content': API_CONTENT}
 
 
-def test_actions_present():
-    with HTTMock(api_mock):
-        api = ManageIQClient('https://example/api', ('admin', 'admin'), verify_ssl=False)
-    with HTTMock(services_mock):
-        services = api.collections.services
-        services.reload()
-        assert hasattr(services.action.create, 'POST')
-        assert hasattr(services.action.edit, 'POST')
-        assert not hasattr(services.action.edit, 'PATCH')
-        assert not hasattr(services.action.edit, 'PUT')
-        assert hasattr(services.action.delete, 'POST')
-    with HTTMock(service_mock):
-        service = api.collections.services[0]
-        assert hasattr(service.action.edit, 'PATCH')
-        assert hasattr(service.action.edit, 'POST')
-        assert hasattr(service.action.edit, 'PUT')
-        assert hasattr(service.action.delete, 'POST')
-        assert hasattr(service.action.delete, 'DELETE')
+@all_requests
+def notfound_mock(url, request):
+    return {'status_code': 404,
+            'content': NOTFOUND_RESPONSE}
 
 
-def test_edit_patch(service, captured_log):
-    with HTTMock(service_mock):
-        outcome = service.action.edit.PATCH({
-            "action": "edit",
-            "path": "name",
-            "value": "new"
-        })
-        assert outcome.id == service.id
-    assert '[RESTAPI] PATCH' in captured_log.getvalue()
-    assert service.collection._api.response.request.method == 'PATCH'
+@all_requests
+def interror_mock(url, request):
+    return {'status_code': 500,
+            'content': ''}
 
 
-def test_edit_post(service, captured_log):
-    with HTTMock(service_mock):
-        outcome = service.action.edit.POST(name='new')
-        assert outcome.id == service.id
-    assert '[RESTAPI] POST' in captured_log.getvalue()
-    response = service.collection._api.response
-    assert response.request.method == 'POST'
-    assert 'action' in response.request.body
-    assert 'resource' in response.request.body
+@all_requests
+def empty_response_mock(url, request):
+    return {'status_code': 200,
+            'content': ''}
 
 
-def test_edit_put(service, captured_log):
-    with HTTMock(service_mock):
-        outcome = service.action.edit.PUT(name='new')
-        assert outcome.id == service.id
-    assert '[RESTAPI] PUT' in captured_log.getvalue()
-    response = service.collection._api.response
-    assert response.request.method == 'PUT'
-    assert 'action' not in response.request.body
-    assert 'resource' not in response.request.body
+@all_requests
+def invalid_response_mock(url, request):
+    return {'status_code': 500,
+            'content': 'invalid'}
+
+
+class TestActionMethods(object):
+    def test_actions_present(self, api):
+        with HTTMock(services_mock):
+            services = api.collections.services
+            services.reload()
+            assert hasattr(services.action.create, 'POST')
+            assert hasattr(services.action.edit, 'POST')
+            assert not hasattr(services.action.edit, 'PATCH')
+            assert not hasattr(services.action.edit, 'PUT')
+            assert hasattr(services.action.delete, 'POST')
+        with HTTMock(service_mock):
+            service = api.collections.services[0]
+            assert hasattr(service.action.edit, 'PATCH')
+            assert hasattr(service.action.edit, 'POST')
+            assert hasattr(service.action.edit, 'PUT')
+            assert hasattr(service.action.delete, 'POST')
+            assert hasattr(service.action.delete, 'DELETE')
+
+    def test_edit_patch(self, service, captured_log):
+        with HTTMock(service_mock):
+            outcome = service.action.edit.PATCH({
+                "action": "edit",
+                "path": "name",
+                "value": "new"
+            })
+            assert outcome.id == service.id
+        assert '[RESTAPI] PATCH' in captured_log.getvalue()
+        assert service.collection._api.response.request.method == 'PATCH'
+
+    def test_edit_post(self, service, captured_log):
+        with HTTMock(service_mock):
+            outcome = service.action.edit.POST(name='new')
+            assert outcome.id == service.id
+        assert '[RESTAPI] POST' in captured_log.getvalue()
+        response = service.collection._api.response
+        assert response.request.method == 'POST'
+        assert 'action' in response.request.body
+        assert 'resource' in response.request.body
+
+    def test_edit_put(self, service, captured_log):
+        with HTTMock(service_mock):
+            outcome = service.action.edit.PUT(name='new')
+            assert outcome.id == service.id
+        assert '[RESTAPI] PUT' in captured_log.getvalue()
+        response = service.collection._api.response
+        assert response.request.method == 'PUT'
+        assert 'action' not in response.request.body
+        assert 'resource' not in response.request.body
+
+
+class TestFailedResults(object):
+    def test_notfound(self, api):
+        with HTTMock(notfound_mock), pytest.raises(APIException) as excinfo:
+            api.get('https://example/api/services/21')
+        assert 'ActiveRecord::RecordNotFound' in str(excinfo.value)
+        assert api.response.status_code == 404
+
+    def test_interror(self, service):
+        with HTTMock(interror_mock), pytest.raises(APIException) as excinfo:
+            service.action.edit.POST(foo='bar')
+        assert 'Request failed with status 500' in str(excinfo.value)
+        assert service.collection._api.response.status_code == 500
+
+    def test_empty_get(self, api):
+        with HTTMock(empty_response_mock), pytest.raises(APIException) as excinfo:
+            api.get('https://example/api/services/21')
+        assert 'JSONDecodeError: empty result' in str(excinfo.value)
+        assert api.response.status_code == 200
+
+    def test_invalid_response(self, api):
+        with HTTMock(invalid_response_mock), pytest.raises(APIException) as excinfo:
+            api.get('https://example/api/services/21')
+        assert 'JSONDecodeError: invalid' in str(excinfo.value)
+        assert api.response.status_code == 500
